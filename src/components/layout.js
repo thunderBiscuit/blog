0bdcdb781163b6eb8a20d18f25fb8368e0e3d373
thunderBiscuit <thunderB@protonmail.com>
2019-11-06 11:59:23 -0500

    remove reliance on material-ui in favour of bulma

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,38 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Container, Grid } from "@material-ui/core"
 
 export default ({ children }) => (
-  <>
-    <Container maxWidth="md" className="maincontainer">
-      <Grid container>
-        <Grid item xs={3}>
-          <Link to={`/`}>
-            <h3
-              style={{
-                marginBottom: "3rem",
-                borderBottom: "2px solid #209cee",
-                marginRight: "2.5rem",
-                color: "#363636",
-                fontFamily: "Fira Sans, sans-serif",
-              }}
-            >
-              thunder-blog
-            </h3>
-          </Link>
-          <Link to={`/`}>About</Link>
-          <br />
-          <Link to={`/all-posts/`}>All Posts</Link>
-          <br />
-          <Link to={`/tags/`}>Tags</Link>
-          <br />
-          <Link to={`/contact/`}>Contact</Link>
-        </Grid>
-        <Grid item xs={9} className="content">
-          {/* <Grid item xs={9}> */}
-          {children}
-        </Grid>
-      </Grid>
-    </Container>
-  </>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <Link to={`/`}>
+          <h3
+            style={{
+              marginBottom: "3rem",
+              borderBottom: "2px solid #209cee",
+              marginRight: "2.5rem",
+              color: "#363636",
+              fontFamily: "Fira Sans, sans-serif",
+            }}
+          >
+            thunder-blog
+          </h3>
+        </Link>
+        <Link to={`/`}>About</Link>
+        <br />
+        <Link to={`/all-posts/`}>All Posts</Link>
+        <br />
+        <Link to={`/tags/`}>Tags</Link>
+        <br />
+        <Link to={`/contact/`}>Contact</Link>
+      </div>
+      <div class="column content">{children}</div>
+    </div>
+  </div>
 )
