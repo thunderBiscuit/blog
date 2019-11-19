a620a259cc2949bf40dd804086411d8b46991201
thunderBiscuit <thunderB@protonmail.com>
2019-11-19 17:11:06 -0500

    add github account link to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,11 +8,15 @@ export default () => (
     <Title>Contact me</Title>
     <p>
       The easiest way to contact me is to{" "}
-      <a href="https://twitter.com/thunderB__">send me a DM on twitter</a>.
+      <a href="https://twitter.com/thunderB__" target="_blank" rel="noopener noreferrer">send me a DM on twitter</a>.
     </p>
     <p>
       You can also send me an email at{" "}
       <a href="mailto: thunderB@protonmail.com">thunderB@protonmail.com</a>.
+    </p>
+    <p>
+      If you want to see what I'm up to, check out my GiHub account
+      <a href="https://github.com/thunderBiscuit" target="_blank" rel="noopener noreferrer"> here</a>.
     </p>
   </Layout>
 )
