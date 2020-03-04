4afca7f4428ff4236ff9bf24401c7feefddf0c6f
thunderBiscuit <thunderB@protonmail.com>
2020-03-04 10:58:22 -0500

    add section for 2020 posts

--- src/pages/all-posts.js
+++ src/pages/all-posts.js
@@ -11,10 +11,22 @@ export default ({ data }) => (
 
     <div style={{ marginTop: "4rem" }}>
       <h5 className="title is-5" style={{ marginBottom: "2rem" }}>
+        2020
+        <span style={{ color: "#bdbdbd" }}>
+          {" "}
+          — {data.twenty.totalCount} Posts
+        </span>
+      </h5>
+      {data.twenty.edges.map(({ node }) => (
+        <PostLink node={node} />
+      ))}
+    </div>
+    <div style={{ marginTop: "7rem" }}>
+      <h5 className="title is-5" style={{ marginBottom: "2rem" }}>
         2019
         <span style={{ color: "#bdbdbd" }}>
           {" "}
-          — {data.nineteen.totalCount} Posts
+          — {data.twenty.totalCount} Posts
         </span>
       </h5>
       {data.nineteen.edges.map(({ node }) => (
@@ -29,6 +41,24 @@ export const query = graphql`
     nineteen: allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { year: { eq: "2019" } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMM, YYYY")
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+    twenty: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { year: { eq: "2020" } } }
     ) {
       totalCount
       edges {
