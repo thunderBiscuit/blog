d799e1e3f394a43f86ad7a97d4f311f0d641a614
thunderBiscuit <thunderB@protonmail.com>
2020-04-11 15:41:43 -0400

    minor changes to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,16 +7,16 @@ export default () => (
   <Layout>
     <Title>Contact me</Title>
     <p>
-      The easiest way to contact me is to{" "}
-      <a href="https://twitter.com/thunderB__" target="_blank" rel="noopener noreferrer">send me a DM on twitter</a>.
+      You can follow or message me on Twitter at&nbsp;
+      <a href="https://twitter.com/thunderB__" target="_blank" rel="noopener noreferrer">@thunderB__</a>.
     </p>
     <p>
-      You can also send me an email at{" "}
-      <a href="mailto: thunderB@protonmail.com">thunderB@protonmail.com</a>.
+      You can also send me an email at&nbsp;
+      <a href="mailto: thunderbiscuit@protonmail.com">thunderbiscuit@protonmail.com</a>.
     </p>
     <p>
-      If you want to see what I'm up to, check out my GiHub account{" "}
-      <a href="https://github.com/thunderBiscuit" target="_blank" rel="noopener noreferrer">here</a>.
+      If you want to see what I'm up to, check out my&nbsp;
+      <a href="https://github.com/thunderBiscuit" target="_blank" rel="noopener noreferrer">GitHub account</a>.
     </p>
   </Layout>
 )
